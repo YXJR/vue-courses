42c89d3a4470458737cb48a0d7a1f0628922640b
zhangyuli <846615152@qq.com>
2020-09-09 10:02:28 +0800

    add vuex plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './user'
+import persist from './plugins/persist'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   modules: {
     user,
   },
+  plugins: [persist],
 })
